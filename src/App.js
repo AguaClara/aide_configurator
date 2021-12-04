fa20ceb4c6e79c7ed509f08f9109d4a27731fc24
Mahi Nair <mahifreya@gmail.com>
2021-12-04 16:38:40 -0500

    styling + submit button works

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Form from "@rjsf/core";
 import ConfigJSONForm from './ConfigJSONForm';
 import {useState} from 'react'
 import $ from "jquery";
+var JSONPretty = require('react-json-pretty');
 
 
 function App() {
 
 const [schema, setSchema] = useState('');
+const [data, setData] = useState ({});
 
 const additional = {
   title: "Edit Onshape Configuration",
@@ -15,6 +17,8 @@ const additional = {
 }
 
 const updatedSchema = $.extend( additional,schema);
+
+const onSubmit = ({formData}, e) => setData(formData);
 
 // default: "A new task"},
   return (
@@ -26,17 +30,22 @@ const updatedSchema = $.extend( additional,schema);
         <ConfigJSONForm
         value={schema}
         callback={setSchema}/> : 
-        <div>
-        <Form schema={
-          updatedSchema
-        }
-        onChange={console.log("changed")}
-        onSubmit={console.log("submitted")}
-        onError={console.log("errors")} />
+          <div className = "rowC">
+            <div></div>
+            <Form 
+              schema={updatedSchema}
+              onChange={console.log("changed")}
+              onSubmit={onSubmit}
+              onError={console.log("errors")} 
+            />
+            {Object.keys(data).length === 0 ?
+            <div> </div> : 
+            <div className = "json-pretty"> 
+              <JSONPretty id="json-pretty" data={data}></JSONPretty> 
+            </div>}
         </div>
     }
-    </div>
-    
+    </div> 
   );
 }
 
